256524176f0845828aca0df75bb8fa4279990ce4
ariannadrovandi <ariannadrovandi02@gmail.com>
2023-10-26 16:26:02 +0200

    fixing nav

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -3,33 +3,35 @@
 
 @section('content')
     <header>
-        <div class="row">
-            <div class="col-12">
-                <nav class="navbar navbar-expand-lg navbar-light bg-light">
-                    <div class="container">
-                        <a class="navbar-brand" href="#"><img src="{{ asset('/img/logo.png') }}" alt="Logo" height="70"></a>
+        <nav class="navbar navbar-expand-lg">
+            <div class="container">
+                <a class="navbar-brand" href="#"><img src="{{ asset('/img/logo.png') }}" alt="Logo" height="70"></a>
 
-                        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav"
-                                aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-                            <span class="navbar-toggler-icon"></span>
-                        </button>
+                <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav"
+                        aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                    <span class="navbar-toggler-icon"></span>
+                </button>
 
-                        <div class="collapse navbar-collapse justify-content-end" id="navbarNav">
-                            <ul class="navbar-nav">
-                                <li class="nav-item">
-                                    <button class="btn btn-outline-light"><a class="nav-link" href="#">Accedi</a></button>
-                                </li>
-                                <li class="nav-item">
-                                    <button class="btn btn-outline-light"><a class="nav-link" href="#">Registrati</a></button>
-                                </li>
-                            </ul>
-                        </div>
-                    </div>
-                </nav>
+                <div class="collapse navbar-collapse justify-content-end" id="navbarNav">
+                    <ul class="navbar-nav">
+                        <li class="nav-item">
+                            <button class="btn btn-outline-light"><a class="nav-link" href="#">Accedi</a></button>
+                        </li>
+                        <li class="nav-item">
+                            <button class="btn btn-outline-light"><a class="nav-link" href="#">Registrati</a></button>
+                        </li>
+                    </ul>
+                </div>
             </div>
-        </div>
+        </nav>
     </header>
     
-
+    <main>
+        <div class="row">
+           <div class="col-12">
+           
+           </div>
+        </div>
+    </main>
      
 @endsection
