a19e7f88e94a31f3ec32f921916458d5e34c3246
francescotrudu <francescotrudu84@gmail.com>
2023-10-27 18:26:38 +0200

    Added breeze

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,10 +1,3 @@
 @extends('layouts.app')
-@section('title') {{'Welcome'}} @endsection
-
 @section('content')
-    <div class="row">
-        <div class="col-12">
-            <h1>Pagina welcome che rimanderà al login</h1>
-        </div>
-    </div>
 @endsection
